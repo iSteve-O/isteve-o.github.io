7ea3a23341fa14254fa9f1e11e2c2e7f9ccd46a5
iSteve-O <isteve-o.ios@gmail.com>
2016-09-12 01:42:33 +0000

    projects

---
+++ packages.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+dpkg-deb -bZgzip /projects/insanelyi App - iOS9 debs
+dpkg-deb -bZgzip /projects/insanelyi Setup debs
+dpkg-deb -bZgzip /projects/MatteTerm debs
+dpkg-deb -bZgzip /projects/MatteTerm for iPad debs
