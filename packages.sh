cb96d2b1288df576f6d0ae42502465b1e632c62f
iSteve-O <iSteve-O@users.noreply.github.com>
2019-12-15 21:45:33 -0500

    Update12
    December 2019 update

--- packages.sh
+++ packages.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-dpkg-deb -bZgzip /projects/insanelyiApp-iOS9 debs
-dpkg-deb -bZgzip /projects/insanelyiSetup debs
-dpkg-deb -bZgzip /projects/MatteTerm debs
-dpkg-deb -bZgzip /projects/MatteTermforiPad debs
+dpkg-deb -bZgzip /projects/Stocker13 debs
